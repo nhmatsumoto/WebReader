454ece332aec543c3e34e8273fe5a8842a6da71c
Hiro Matsumoto <hiroyukims@hotmail.com>
2022-07-06 01:22:09 -0300

    Algoritmo base

--- Models/Reader.cs
+++ Models/Reader.cs
@@ -1,1 +1,31 @@
-public record Reader(Guid Id, String FullText);
+using System;
+using System.Text.RegularExpressions;
+public class Reader
+{
+    protected Guid Id { get; set; }
+    protected String Text { get; set; }
+
+    public Reader(String text)
+    {
+        Id = Guid.NewGuid();
+        Text = text;
+    }
+
+    public static async Task Print(IEnumerable<String> words, int timeRate)
+    {
+        int counter = words.Count();
+        int index = 0;
+        string[] nextWord = words.ToArray();
+
+        while (counter < index)
+        {
+            Thread.Sleep(timeRate);
+            Console.WriteLine($"{nextWord[index]}");
+            index++;
+        }
+
+        Console.WriteLine("FIM!");
+    }
+    public IEnumerable<String> GetWords() => Regex.Split(Text, @"[^\w0-9-]+").Where(x => !String.IsNullOrWhiteSpace(x));
+
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,34 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Configura serviços
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => new Reader(Guid.NewGuid(), "Reader1 Reader2 Reader3"));
+// Configura pipeline HTTP request
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapGet("/read-text/", async (String text) =>
+{
+    var reader = new Reader(text);
+    var words = reader.GetWords();
+    var result = await Reader.Print(words, 1000);
+    return result;
+});
 
 app.Run();
