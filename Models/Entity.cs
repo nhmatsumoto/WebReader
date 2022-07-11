cb14b7e6126f88ab1031441aff94605743602e3a
Hiro Matsumoto <hiroyukims@hotmail.com>
2022-07-11 00:30:44 -0300

    fix jwt system and add bootrap class for register services

--- Models/Entity.cs
+++ Models/Entity.cs
@@ -1,10 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
 namespace WebReader.Models
 {
     public abstract class Entity
     {
+        [HiddenInput(DisplayValue = false)]
         public Guid Id { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
         public DateTime InsertTimestamp { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
         public DateTime UpdateTimestamp { get; set; }
+
         public bool Active { get; set; }
 
         public Entity() => Id = Guid.NewGuid();
--- Program.cs
+++ Program.cs
@@ -1,101 +1,23 @@
-using Services.WebReader;
 using Data;
 using Microsoft.EntityFrameworkCore;
 using WebReader.Models;
-using Microsoft.OpenApi.Models;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using WebReader;
 
+//BUILDER
 var builder = WebApplication.CreateBuilder(args);
 
-var securityScheme = new OpenApiSecurityScheme()
-{
-    Name = "Authorization",
-    Type = SecuritySchemeType.ApiKey,
-    Scheme = "Bearer",
-    BearerFormat = "JWT",
-    In = ParameterLocation.Header,
-    Description = "JSON Web Token based security",
-};
+//ADD SERVICES
+Bootstrap.AddApplicationServices(builder);
 
-var contact = new OpenApiContact()
-{
-    Name = "Nilton H. Matsumoto",
-    Email = "hiroyukims@hotmail.com",
-    Url = new Uri("https://nhmatsumoto.github.io/")
-};
-
-var info = new OpenApiInfo()
-{
-    Version = "v1",
-    Title = "WebReader demo",
-    Description = "WebReader is a software for making support to farmers",
-    //TermsOfService = new Uri(""),
-    Contact = contact
-};
-
-var securityReq = new OpenApiSecurityRequirement()
-{
-    {
-        new OpenApiSecurityScheme
-        {
-            Reference = new OpenApiReference
-            {
-                Type = ReferenceType.SecurityScheme,
-                Id = "Bearer"
-            }
-        },
-        new string[] {}
-    }
-};
-
-// Configura serviços
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(o =>
-{
-    o.SwaggerDoc("v1", info);
-    o.AddSecurityDefinition("Bearer", securityScheme);
-    o.AddSecurityRequirement(securityReq);
-});
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<WebReaderDataContext>(options =>
-    options.UseSqlServer("Server=localhost;Database=sa;Trusted_Connection=True;")
-);
-
-// Add JWT configuration
-builder.Services.AddAuthentication(o =>
-{
-    o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    o.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(o =>
-{
-    o.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey
-            (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = false,
-        ValidateIssuerSigningKey = true
-    };
-});
-
-builder.Services.AddAuthorization();
-builder.Services.AddEndpointsApiExplorer();
-
+//APPLICATION
 var app = builder.Build();
 
-// Configura pipeline HTTP request
+//ENABLE SWAGGER WHEN APP RUNNING IN DEVELOPMENT ENVIROMENT
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -108,35 +30,10 @@ app.UseDefaultFiles();
 app.UseStaticFiles();
 app.UseHttpsRedirection();
 
-//Endpoints
-app.MapGet("/climate/", async (WebReaderDataContext db) => await db.Climates.ToListAsync());
 
-app.MapPost("/climate/add/data", async (WebReaderDataContext db, Climate data) =>
+//GET TOKEN
+app.MapPost("/webreader/security/login", [AllowAnonymous] (UserDto user) =>
 {
-    if (data != null)
-    {
-        await db.Climates.AddAsync(data);
-        await db.SaveChangesAsync();
-        return Results.Created($"/climate/{data.Id}", data);
-    }
-
-    return Results.BadRequest();
-});
-
-app.MapPost("/add-timer/", (TimeSpan time) =>
-{
-    DateTime.Now.Add(time);
-});
-
-app.MapGet("/read-text/", (String text) =>
-{
-    var reader = new Reader(text);
-    reader.Print(700);
-});
-
-app.MapPost("/security/getToken", [AllowAnonymous] (UserDto user) =>
-{
-
     if (user.email == "admin" && user.password == "adminpassword")
     {
         var issuer = builder.Configuration["Jwt:Issuer"];
@@ -176,11 +73,21 @@ app.MapPost("/security/getToken", [AllowAnonymous] (UserDto user) =>
     }
 });
 
-app.MapGet("/climates", [Authorize] async (WebReaderDataContext db) =>
+//GET ALL CLIMATES
+app.MapGet("/climate/", [Authorize] async (WebReaderDataContext db) =>
 {
-    return await db.Climates.ToListAsync();
+    var climates = await db.Climates.ToListAsync();
+    return climates == null ? Results.NotFound() : Results.Ok(climates);
 });
 
+//GET CLIMATE BY ID
+app.MapGet("/climates/{id}", [Authorize] async (WebReaderDataContext db, Guid id) =>
+{
+    var climate = await db.Climates.FirstOrDefaultAsync(x => x.Id == id);
+    return climate == null ? Results.NotFound() : Results.Ok(climate);
+});
+
+//INSERT NEW CLIMATE
 app.MapPost("/climates", [Authorize] async (WebReaderDataContext db, Climate climate) =>
 {
     if (await db.Climates.FirstOrDefaultAsync(x => x.Id == climate.Id) != null)
@@ -193,12 +100,7 @@ app.MapPost("/climates", [Authorize] async (WebReaderDataContext db, Climate cli
     return Results.Created($"/Items/{climate.Id}", climate);
 });
 
-app.MapGet("/climates/{id}", [Authorize] async (WebReaderDataContext db, Guid id) =>
-{
-    var climate = await db.Climates.FirstOrDefaultAsync(x => x.Id == id);
-    return climate == null ? Results.NotFound() : Results.Ok(climate);
-});
-
+//UPDATE
 app.MapPut("/climates/{id}", [Authorize] async (WebReaderDataContext db, Guid id, Climate climate) =>
 {
     var existItem = await db.Climates.FirstOrDefaultAsync(x => x.Id == id);
@@ -213,10 +115,7 @@ app.MapPut("/climates/{id}", [Authorize] async (WebReaderDataContext db, Guid id
 app.MapDelete("/climates/{id}", [Authorize] async (WebReaderDataContext db, Guid id) =>
 {
     var existItem = await db.Climates.FirstOrDefaultAsync(x => x.Id == id);
-    if (existItem == null)
-    {
-        return Results.BadRequest();
-    }
+    if (existItem == null) return Results.BadRequest();
 
     db.Climates.Remove(existItem);
     await db.SaveChangesAsync();
